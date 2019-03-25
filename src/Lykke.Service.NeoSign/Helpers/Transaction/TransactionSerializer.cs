0845ee4ac1b9c3f2c357c350815df2ebbe70ad0f
some-dev <avolkov.dev@gmail.com>
2019-03-25 14:05:59 +0300

    fix minor debug issues

--- src/Lykke.Service.NeoSign/Helpers/Transaction/TransactionSerializer.cs
+++ src/Lykke.Service.NeoSign/Helpers/Transaction/TransactionSerializer.cs
@@ -10,7 +10,8 @@ namespace Lykke.Service.NeoSign.Helpers.Transaction
     {
         public static string Serialize(NeoModules.NEP6.Transactions.Transaction transaction, TransactionType type)
         {
-            return TransactionTypeWrapperContract.Create(type, TransactionContract.FromDomain(transaction).ToJson()).ToJson().ToBase64());
+            return TransactionTypeWrapperContract.Create(type, TransactionContract.FromDomain(transaction).ToJson())
+                .ToJson().ToBase64();
         }
 
         public static (NeoModules.NEP6.Transactions.Transaction transaction, TransactionType type) Deserialize(string source)
